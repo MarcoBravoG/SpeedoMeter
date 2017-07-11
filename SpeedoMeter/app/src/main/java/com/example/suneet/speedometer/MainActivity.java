07be3906541504159a08257d9361d2c294d64474
codedsun <suneetbond91@gmail.com>
2017-07-11 23:23:06 +0530

    Speedometer gauge added

--- SpeedoMeter/app/src/main/java/com/example/suneet/speedometer/MainActivity.java
+++ SpeedoMeter/app/src/main/java/com/example/suneet/speedometer/MainActivity.java
@@ -5,7 +5,10 @@ import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.github.anastr.speedviewlib.base.Gauge;
 
 import java.util.Locale;
 
@@ -20,6 +23,14 @@ public class MainActivity extends Activity {
         appName= (TextView) findViewById(R.id.appName);
         Typeface typeface=Typeface.createFromAsset(am,String.format(Locale.US,"fonts/%s","PoiretOne-Regular.ttf"));
         appName.setTypeface(typeface);
+        Gauge gauge= (Gauge) findViewById(R.id.speedometer);
+        gauge.setTextTypeface(typeface);
+        gauge.setSpeedTextTypeface(typeface);
+        gauge.setTextSize(24);
+        gauge.setSpeedTextSize(32);
+        gauge.setUnit("KM/HR");
+        
+
 
 
     }
