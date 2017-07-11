dafff6d2b47094097aa485fc2a024833459bd577
codedsun <suneetbond91@gmail.com>
2017-07-11 11:18:35 +0530

    Added menu

--- SpeedoMeter/app/src/main/java/com/example/suneet/speedometer/MainActivity.java
+++ SpeedoMeter/app/src/main/java/com/example/suneet/speedometer/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.suneet.speedometer;
 
+import android.app.Activity;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends Activity {
+    TextView appName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AssetManager am=this.getAssets();
+        appName= (TextView) findViewById(R.id.appName);
+        Typeface typeface=Typeface.createFromAsset(am,String.format(Locale.US,"fonts/%s","PoiretOne-Regular.ttf"));
+        appName.setTypeface(typeface);
+
+
     }
 }
